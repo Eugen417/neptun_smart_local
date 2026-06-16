9fc367b7479aaef546ba686f32fd041e5d586228
Eugen417 <eugen417@gmail.com>
2026-06-16 18:31:48 +0300

    Update sensor.py

--- custom_components/neptun_smart_local/sensor.py
+++ custom_components/neptun_smart_local/sensor.py
@@ -1,241 +1,122 @@
 from __future__ import annotations
 
+from collections import namedtuple
 from datetime import timedelta
-from homeassistant.components.binary_sensor import BinarySensorEntity, BinarySensorDeviceClass
+
 from homeassistant.helpers.entity import EntityCategory
-
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorEntity,
+    SensorStateClass,
+)
+from homeassistant.const import (UnitOfVolume, PERCENTAGE)
 from .const import DOMAIN
-from . import NeptunSmart
-from .device import WirelessSensor
-
-SCAN_INTERVAL = timedelta(seconds=10)
+from .device import NeptunSmart, WirelessSensor, Counter
 
 async def async_setup_entry(HomeAssistant, config_entry, async_add_entities):
     device: NeptunSmart = HomeAssistant.data[DOMAIN][config_entry.entry_id]
-    binary_sensors = []
-    binary_sensors.append(MainModule(device=device))
-    binary_sensors.append(FirstGroupModuleAlert(device))
-    
-    if device.get_dual_group_mode():
-        binary_sensors.append(SecondGroupModuleAlert(device))
-        
-    binary_sensors.append(DischargeWirelessSensors(device))
-    binary_sensors.append(LostWirelessSensors(device))
-    
-    for i in 1, 2, 3, 4:
-        binary_sensors.append(WiredLineAlertStatus(device=device, line_number=i))
-        
+    sensors = []
+    sensors.append(WirelessSensorsConnected(device=device))
     for i in range(0, device.get_number_of_connected_wireless_sensors()):
-        binary_sensors.append(WirelessSensorAlertStatus(device, i+1, device.wireless_sensors[i]))
-        binary_sensors.append(WirelessSensorDischargeStatus(device, i + 1, device.wireless_sensors[i]))
-        binary_sensors.append(WirelessSensorLostStatus(device, i + 1, device.wireless_sensors[i]))
-        
-    async_add_entities(binary_sensors, update_before_add=False)
+        sensors.append(WirelessSensorsBatteryLevel(device, i+1, device.wireless_sensors[i]))
+        sensors.append(WirelessSensorsSignalLevel(device, i+1, device.wireless_sensors[i]))
+    for counter in device.counters:
+        sensors.append(CounterSensor(device, counter))
+    async_add_entities(sensors, update_before_add=False)
 
 
-class MainModule(BinarySensorEntity):
-    _attr_device_class = BinarySensorDeviceClass.PROBLEM
-
+class WirelessSensorsConnected(SensorEntity):
     def __init__(self, device: NeptunSmart):
         self._device = device
-        self._attr_unique_id = self._device.get_name()
-        self._attr_name = self._device.get_name()
+        self._attr_unique_id = f"{device.get_name()}_Wireless_sensors_connected"
+        self._attr_name = "Подключено радиодатчиков"
+        self._attr_entity_category = EntityCategory.DIAGNOSTIC
+        self._attr_native_value = self._device.get_number_of_connected_wireless_sensors()
+
+    async def async_update(self) -> None:
+        self._attr_native_value = self._device.get_number_of_connected_wireless_sensors()
 
     @property
     def device_info(self):
         return {
-            "identifiers": {(DOMAIN, self._device.get_name())},
-            "name": self._device.get_name(),
-            "model": "Neptun Smart",
-            "manufacturer": "Teploluxe",
+            "identifiers": {(DOMAIN, self._device.get_name())}
         }
 
     @property
     def icon(self):
-        return "mdi:water-pump"
-
-    @property
-    def is_on(self) -> bool:
-        return self._device.get_first_group_alarm() or self._device.get_second_group_alarm()
+        return "mdi:sun-wireless-outline"
 
 
-class FirstGroupModuleAlert(BinarySensorEntity):
-    _attr_device_class = BinarySensorDeviceClass.MOISTURE
-
-    def __init__(self, device: NeptunSmart):
+class WirelessSensorsBatteryLevel(SensorEntity):
+    def __init__(self, device: NeptunSmart, sensor_number, sensor: WirelessSensor):
         self._device = device
-        self._attr_unique_id = f"{device.get_name()}_first_group_alarm_module_alert"
-        self._attr_name = "Протечка 1 группы"
-
-    @property
-    def device_info(self):
-        return {"identifiers": {(DOMAIN, self._device.get_name())}}
-
-    @property
-    def icon(self):
-        return "mdi:water"
-
-    @property
-    def is_on(self) -> bool:
-        return self._device.get_first_group_alarm()
-
-
-class SecondGroupModuleAlert(BinarySensorEntity):
-    _attr_device_class = BinarySensorDeviceClass.MOISTURE
-
-    def __init__(self, device: NeptunSmart):
-        self._device = device
-        self._attr_unique_id = f"{device.get_name()}_second_group_alarm_module_alert"
-        self._attr_name = "Протечка 2 группы"
-
-    @property
-    def device_info(self):
-        return {"identifiers": {(DOMAIN, self._device.get_name())}}
-
-    @property
-    def icon(self):
-        return "mdi:water"
-
-    @property
-    def is_on(self) -> bool:
-        return self._device.get_second_group_alarm()
+        self._sensor_number = sensor_number
+        self._sensor = sensor
+        self._attr_unique_id = f"{device.get_name()}_WirelessSensors{sensor_number}BatteryLevel"
+        self._attr_name = f"Заряд батареи радиодатчика {sensor_number}"
+        self._attr_device_class = SensorDeviceClass.BATTERY
+        self._attr_native_unit_of_measurement = PERCENTAGE
+        self._attr_entity_category = EntityCategory.DIAGNOSTIC
+        self._attr_native_value = self._sensor.get_battery_level()
 
     async def async_update(self) -> None:
-        self._attr_available = self._device.get_dual_group_mode()
-
-
-class DischargeWirelessSensors(BinarySensorEntity):
-    _attr_device_class = BinarySensorDeviceClass.BATTERY
-
-    def __init__(self, device: NeptunSmart):
-        self._device = device
-        self._attr_unique_id = f"{device.get_name()}_discharge_wireless_sensors"
-        self._attr_name = "Разряд батареи радиодатчиков"
+        self._attr_native_value = self._sensor.get_battery_level()
 
     @property
     def device_info(self):
-        return {"identifiers": {(DOMAIN, self._device.get_name())}}
+        return {
+            "identifiers": {(DOMAIN, self._device.get_name())}
+        }
 
     @property
     def icon(self):
-        return "mdi:battery"
-
-    @property
-    def is_on(self) -> bool:
-        return self._device.get_discharge_wireless_sensors()
+        return "mdi:battery-high"
 
 
-class LostWirelessSensors(BinarySensorEntity):
-    _attr_device_class = BinarySensorDeviceClass.CONNECTIVITY
-
-    def __init__(self, device: NeptunSmart):
-        self._device = device
-        self._attr_unique_id = f"{device.get_name()}_lost_wireless_sensors"
-        self._attr_name = "Потеря связи с радиодатчиками"
-
-    @property
-    def device_info(self):
-        return {"identifiers": {(DOMAIN, self._device.get_name())}}
-
-    @property
-    def icon(self):
-        return "mdi:wifi"
-
-    @property
-    def is_on(self) -> bool:
-        return self._device.get_lost_wireless_sensors()
-
-
-class WiredLineAlertStatus(BinarySensorEntity):
-    _attr_device_class = BinarySensorDeviceClass.MOISTURE
-
-    def __init__(self, device: NeptunSmart, line_number):
-        self._device = device
-        self._line_number = line_number
-        self._attr_unique_id = f"{device.get_name()}_WiredAlertStatus_line{line_number}"
-        self._attr_name = f"Протечка проводной линии {line_number}"
-        self._attr_entity_category = EntityCategory.DIAGNOSTIC
-
-    @property
-    def device_info(self):
-        return {"identifiers": {(DOMAIN, self._device.get_name())}}
-
-    @property
-    def icon(self):
-        return "mdi:water"
-
-    @property
-    def is_on(self) -> bool:
-        return self._device.get_line_status(line_number=self._line_number)
-
-
-class WirelessSensorAlertStatus(BinarySensorEntity):
-    _attr_device_class = BinarySensorDeviceClass.MOISTURE
-
-    def __init__(self, device: NeptunSmart, sensor_number, sensor: WirelessSensor):
+class WirelessSensorsSignalLevel(SensorEntity):
+    def __init__(self,device: NeptunSmart, sensor_number, sensor: WirelessSensor):
         self._device = device
         self._sensor_number = sensor_number
         self._sensor = sensor
-        self._attr_unique_id = f"{device.get_name()}_WirelessAlertStatus_sensor{sensor_number}"
-        self._attr_name = f"Протечка радиодатчика {sensor_number}"
+        self._attr_unique_id = f"{device.get_name()}_WirelessSensors{sensor_number}SignalLevel"
+        self._attr_name = f"Уровень сигнала радиодатчика {sensor_number}"
         self._attr_entity_category = EntityCategory.DIAGNOSTIC
+        self._attr_native_value = self._sensor.get_signal_level()
+
+    async def async_update(self) -> None:
+        self._attr_native_value = self._sensor.get_signal_level()
 
     @property
     def device_info(self):
-        return {"identifiers": {(DOMAIN, self._device.get_name())}}
+        return {
+            "identifiers": {(DOMAIN, self._device.get_name())}
+        }
 
     @property
     def icon(self):
-        return "mdi:water"
-
-    @property
-    def is_on(self) -> bool:
-        return self._sensor.get_alert_status()
+        return "mdi:signal"
 
 
-class WirelessSensorDischargeStatus(BinarySensorEntity):
-    _attr_device_class = BinarySensorDeviceClass.BATTERY
-
-    def __init__(self, device: NeptunSmart, sensor_number, sensor: WirelessSensor):
+class CounterSensor(SensorEntity):
+    def __init__(self, device: NeptunSmart, counter: Counter):
         self._device = device
-        self._sensor_number = sensor_number
-        self._sensor = sensor
-        self._attr_unique_id = f"{device.get_name()}_WirelessDischargeStatus_sensor{sensor_number}"
-        self._attr_name = f"Разряд батареи радиодатчика {sensor_number}"
-        self._attr_entity_category = EntityCategory.DIAGNOSTIC
+        self._counter = counter
+        self._attr_unique_id = f"{device.get_name()}_Counter{counter.get_address()}"
+        self._attr_name = f"Счетчик воды {counter.get_address()}"
+        self._attr_native_value = self._counter.get_value()/1000
+        self._attr_native_unit_of_measurement = UnitOfVolume.CUBIC_METERS
+        self._attr_device_class = SensorDeviceClass.WATER
+        self._attr_state_class = SensorStateClass.TOTAL_INCREASING
+
+    async def async_update(self) -> None:
+        self._attr_native_value = self._counter.get_value()/1000
 
     @property
     def device_info(self):
-        return {"identifiers": {(DOMAIN, self._device.get_name())}}
+        return {
+            "identifiers": {(DOMAIN, self._device.get_name())}
+        }
 
     @property
     def icon(self):
-        return "mdi:battery"
-
-    @property
-    def is_on(self) -> bool:
-        return self._sensor.get_discharge_status()
-
-
-class WirelessSensorLostStatus(BinarySensorEntity):
-    _attr_device_class = BinarySensorDeviceClass.CONNECTIVITY
-
-    def __init__(self, device: NeptunSmart, sensor_number, sensor: WirelessSensor):
-        self._device = device
-        self._sensor_number = sensor_number
-        self._sensor = sensor
-        self._attr_unique_id = f"{device.get_name()}_WirelessLostStatus_sensor{sensor_number}"
-        self._attr_name = f"Потеря связи с радиодатчиком {sensor_number}"
-        self._attr_entity_category = EntityCategory.DIAGNOSTIC
-
-    @property
-    def device_info(self):
-        return {"identifiers": {(DOMAIN, self._device.get_name())}}
-
-    @property
-    def icon(self):
-        return "mdi:wifi"
-
-    @property
-    def is_on(self) -> bool:
-        return self._sensor.get_lost_sensor_status()
+        return "mdi:counter"
